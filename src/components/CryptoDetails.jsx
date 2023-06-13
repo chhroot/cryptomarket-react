6bc53ddec4ef6ee0cafa9c9363aa8c227dfac6f4
Amruth Kiran <amruthkiran47@gmail.com>
2023-06-13 20:44:35 +0530

    initial commit

---
+++ src/components/CryptoDetails.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CryptoDetails = () => {
+  return (
+    <div>Cryptodetails</div>
+  )
+}
+
+export default CryptoDetails
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,8 @@ import { Button, Menu, Typography, Avatar } from 'antd';
 import { Link } from 'react-router-dom';
 import { HomeOutlined,  MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined } from '@ant-design/icons';
 
+import icon from '../images/cryptocurrency.png';
+
 const Navbar = () => {
   return (
     <div className='nav-container'>
@@ -13,6 +15,20 @@ const Navbar = () => {
             </Typography.Title>
             {/* <Button className='menu-control-container'></Button> */}
         </div>
+        <Menu theme="dark">
+          <Menu.Item icon={<HomeOutlined />}>
+            <Link to="/">Home</Link>
+          </Menu.Item>
+          <Menu.Item icon={<FundOutlined />}>
+            <Link to="/cryptocurrencies">Cryptocurrencies</Link>
+          </Menu.Item>
+          <Menu.Item icon={<MoneyCollectOutlined />}>
+            <Link to="/exchanges">Exchanges</Link>
+          </Menu.Item>
+          <Menu.Item icon={<BulbOutlined />}>
+            <Link to="/news">News</Link>
+          </Menu.Item>
+        </Menu>
     </div>
   )
 }
