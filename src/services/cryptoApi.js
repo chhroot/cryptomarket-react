3b7a6126fcefb590579d4ff09cec27d7a0079178
Amruth Kiran <amruthkiran47@gmail.com>
2023-06-17 17:29:47 +0530

    ft: search

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -14,7 +14,7 @@ export const cryptoApi = createApi({
   reducerPath: 'cryptoApi',
   baseQuery: fetchBaseQuery({ baseUrl }),
   endpoints: (builder) => ({
-    getCryptos: builder.query({ query: () => createRequest('/coins') }),
+    getCryptos: builder.query({ query: (count) => createRequest(`/coins?limit=${count}`) }),
   }),
 });
 
